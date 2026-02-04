9dbfc167162214d5c4b8931842b81cea4a421eb0
Konstantinos Togias <ktogias@gmail.com>
2026-02-04 16:56:11 +0200

    Wait for Keycloak redirect before cy.origin

--- tests/e2e/login_flow.cy.ts
+++ tests/e2e/login_flow.cy.ts
@@ -4,6 +4,7 @@ const password = Cypress.env('PORTAL_PASSWORD') as string;
 describe('Portal login flow', () => {
   it('completes the Keycloak login and surfaces partner claims', () => {
     cy.visit('/');
+    cy.url({ timeout: 15000 }).should('include', 'https://keycloak.localhost:8443/realms/portal-dev');
 
     cy.origin(
       'https://keycloak.localhost:8443',
@@ -21,19 +22,15 @@ describe('Portal login flow', () => {
 
     cy.visit('/partner/?popup_handshake=true');
 
-    cy.origin(
-      'https://keycloak.localhost:8443',
-      { args: { username, password } },
-      ({ username, password }) => {
-        cy.get('body').then(($body) => {
-          if ($body.find('input#username').length) {
-            cy.get('input#username').type(username);
-            cy.get('input#password').type(password);
-            cy.get('input#kc-login').click();
-          }
-        });
-      }
-    );
+    cy.origin('https://keycloak.localhost:8443', { args: { username, password } }, ({ username, password }) => {
+      cy.get('body').then(($body) => {
+        if ($body.find('input#username').length) {
+          cy.get('input#username').type(username);
+          cy.get('input#password').type(password);
+          cy.get('input#kc-login').click();
+        }
+      });
+    });
 
     cy.contains('Partner Application', { timeout: 15000 }).should('be.visible');
     cy.contains('"username": "demo.user"', { timeout: 15000 }).should('exist');
