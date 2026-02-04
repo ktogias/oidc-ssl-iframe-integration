f1cc630e98a708a240b784eb5eef7b64adc4e2f4
Konstantinos Togias <ktogias@gmail.com>
2026-02-04 13:58:49 +0200

    Fix Keycloak templating

--- infra/keycloak/start.sh
+++ infra/keycloak/start.sh
@@ -6,6 +6,10 @@ TEMPLATE=$IMPORT_DIR/realm-export.json.template
 RENDERED=$IMPORT_DIR/realm-export.json
 TEMP_FILE=$IMPORT_DIR/realm-export.json.tmp
 
+escape_sed() {
+  printf '%s' "$1" | sed -e 's/[\\/&]/\\&/g'
+}
+
 if [[ -f "$TEMPLATE" ]]; then
   : "${DEMO_USER_USERNAME:=demo.user}"
   : "${DEMO_USER_EMAIL:=demo.user@example.com}"
@@ -13,9 +17,21 @@ if [[ -f "$TEMPLATE" ]]; then
   : "${DEMO_USER_LAST_NAME:=User}"
   : "${DEMO_USER_PASSWORD:=changeMe123}"
 
-  VARIABLES='${OIDC_PARTNER_PROXY_SECRET} ${DEMO_USER_USERNAME} ${DEMO_USER_EMAIL} ${DEMO_USER_FIRST_NAME} ${DEMO_USER_LAST_NAME} ${DEMO_USER_PASSWORD}'
+  SECRET=$(escape_sed "$OIDC_PARTNER_PROXY_SECRET")
+  USERNAME=$(escape_sed "$DEMO_USER_USERNAME")
+  EMAIL=$(escape_sed "$DEMO_USER_EMAIL")
+  FIRST_NAME=$(escape_sed "$DEMO_USER_FIRST_NAME")
+  LAST_NAME=$(escape_sed "$DEMO_USER_LAST_NAME")
+  PASSWORD=$(escape_sed "$DEMO_USER_PASSWORD")
 
-  envsubst "$VARIABLES" < "$TEMPLATE" > "$TEMP_FILE"
+  sed -e "s/CHANGE_ME_PARTNER_PROXY_SECRET/${SECRET}/g" \
+      -e "s/__DEMO_USER_USERNAME__/${USERNAME}/g" \
+      -e "s/__DEMO_USER_EMAIL__/${EMAIL}/g" \
+      -e "s/__DEMO_USER_FIRST_NAME__/${FIRST_NAME}/g" \
+      -e "s/__DEMO_USER_LAST_NAME__/${LAST_NAME}/g" \
+      -e "s/__DEMO_USER_PASSWORD__/${PASSWORD}/g" \
+      "$TEMPLATE" > "$TEMP_FILE"
+
   mv "$TEMP_FILE" "$RENDERED"
 fi
 
